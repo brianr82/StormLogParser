4ef370340744f338edfeba61a900e9af265f835d
Brian <brian.ramprasad@gmail.com>
2018-10-16 00:20:59 -0400

    updated parser

--- StormParser.py
+++ StormParser.py
@@ -1,7 +1,20 @@
+import re
+
+
+f = open('sample_storm_log.log', 'r')
+for line in f:
+    if "r.AppendByteBolt" in line and "###################################" not in line:
+        filtered_line = line.split('r.AppendByteBolt')[1].split(' [INFO] ')[1].split(',')
+        if len (filtered_line) > 2:
+            print (filtered_line[0] + ' ' + filtered_line[1] + ' ' + filtered_line[2])
+
 
 
 
 f = open('sample_storm_log.log', 'r')
 for line in f:
     if "r.AppendByteBolt" in line and "###################################" not in line:
-        print(line)
+        filtered_line = line.split('r.AppendByteBolt')[1].split(' [INFO] ')[1].split(',')
+        if len (filtered_line) < 3:
+            print(filtered_line[0]+ ' '+ filtered_line[1])
+
